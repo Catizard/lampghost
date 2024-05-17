ea40fda6f82506d0a477384e2490ff1007e7930b
Catizard <1185032459@qq.com>
2024-05-17 17:27:12 +0800

    add sqlite

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,7 @@ var rootCmd = &cobra.Command{
 	Use: "lampghost",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
+	// TODO: if start with no args, open tui for use
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -39,5 +40,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

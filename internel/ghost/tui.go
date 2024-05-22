3ba66385db31f89bc6f354bffa22956c8325f1dc
Catizard <1185032459@qq.com>
2024-05-22 16:47:48 +0800

    print difficult table levels instead of dummy items

--- internel/ghost/tui.go
+++ internel/ghost/tui.go
@@ -2,6 +2,8 @@ package ghost
 
 import (
 	"log"
+	"sort"
+	"strconv"
 	"time"
 
 	"github.com/Catizard/lampghost/internel/difftable"
@@ -63,12 +65,40 @@ type mainModel struct {
 	choice  item
 }
 
-func newModel(timeout time.Duration, items []list.Item) mainModel {
+func newModel(timeout time.Duration, dth *difftable.DiffTableHeader, diffTable []difftable.DiffTable) mainModel {
+	// convert diffTable to list items
+	levels := make(map[string]interface{})
+	for _, v := range diffTable {
+		levels[v.Level] = new(interface{})
+	}
+	sortedLevels := make([]string, 0)
+	for level := range levels {
+		sortedLevels = append(sortedLevels, level)
+	}
+	sort.Slice(sortedLevels, func(i, j int) bool {
+		ll := sortedLevels[i]
+		rr := sortedLevels[j]
+		ill, errL := strconv.Atoi(ll)
+		irr, errR := strconv.Atoi(rr)
+		if errL == nil && errR == nil {
+			return ill < irr
+		}
+		return ll < rr
+	})
+	items := make([]list.Item, 0)
+	for _, v := range sortedLevels {
+		title := dth.Symbol + " " + v
+		n := item{
+			title: title,
+			desc:  "発狂BMS難易度表" + title,
+		}
+		items = append(items, n)
+	}
 	m := mainModel{state: timerView}
 	m.timer = timer.New(timeout)
 	m.spinner = spinner.New()
 	m.list = list.New(items, list.NewDefaultDelegate(), 0, 0)
-	m.list.Title = "Testtt"
+	m.list.Title = "Levels"
 	m.list.SetShowHelp(false)
 	m.list.SetShowStatusBar(false)
 	m.list.KeyMap.NextPage.Unbind()
@@ -176,32 +206,7 @@ func (m *mainModel) resetSpinner() {
 // left is the specified difficult table's levels
 // right is the related song list and lamp status
 func OpenGhostTui(dth *difftable.DiffTableHeader, dt []difftable.DiffTable, songData []SongData, scoreLog []ScoreLog) {
-	items := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		item{title: "Terrycloth", desc: "In other words, towel fabric"},
-	}
-	if _, err := tea.NewProgram(newModel(defaultTime, items)).Run(); err != nil {
+	if _, err := tea.NewProgram(newModel(defaultTime, dth, dt)).Run(); err != nil {
 		log.Fatal(err)
 	}
 }

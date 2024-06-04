88ed5519bcf9117db773690e6bc44ec81af5fe0c
Catizard <1185032459@qq.com>
2024-06-04 15:52:32 +0800

    feat(course): Ignore mutated courses

--- internal/difftable/courseInfo.go
+++ internal/difftable/courseInfo.go
@@ -10,15 +10,25 @@ import (
 
 // Represents one course
 type CourseInfo struct {
-	Id     int      `db:"id"`
-	Name   string   `json:"name" db:"name"`
-	Md5    []string `json:"md5"`
-	Source string   `db:"source"`
+	Id         int      `db:"id"`
+	Name       string   `json:"name" db:"name"`
+	Md5        []string `json:"md5"`
+	Source     string   `db:"source"`
+	Constraint []string `json:"constraint"`
 	// This field's only purpose is to store value in database
 	// Since you cannot directly store array in database
 	Md5s    string `db:"md5s"`
 	Sha256s string // Can be seen as a mapping from md5s
 }
+
+var (
+	// TODO: Make shouldIgnore as a configurable option
+	shouldIgnoreSpecialConstaints = true
+	ignoreConstraints             = map[string]struct{}{
+		"no_good":  {},
+		"no_speed": {},
+	}
+)
 
 // Initialze course_info header
 func InitCourseInfoTable() error {
@@ -48,17 +58,25 @@ func saveCourseInfoFromTableHeader(tx *sqlx.Tx, dth DiffTableHeader) error {
 		return err
 	}
 
-	// TODO: hack on rawData to delete no good/no speed mutated courses
 	rawData := dth.Course
 	for _, arr := range rawData {
 		for _, v := range arr {
 			v.prepareBeforeSave(dth)
 
 			skipFlag := false
+			// Skip 1: There is a same course exists
 			for _, p := range courseArr {
 				if v.Name == p.Name && v.Md5s == p.Md5s && v.Source == p.Source {
 					skipFlag = true
 					break
+				}
+			}
+			// Skip 2: Open ignore special constraints flag and it matches at least one
+			if shouldIgnoreSpecialConstaints {
+				for _, constraint := range v.Constraint {
+					if _, ok := ignoreConstraints[constraint]; ok {
+						skipFlag = true
+					}
 				}
 			}
 			if skipFlag {

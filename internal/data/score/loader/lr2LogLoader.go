c37bd458e3021a287c0b696b0d23ff13620f44b2
Catizard <1185032459@qq.com>
2024-06-15 21:29:31 +0800

    feat(loader)!: Add filter parameter in load

--- internal/data/score/loader/lr2LogLoader.go
+++ internal/data/score/loader/lr2LogLoader.go
@@ -3,9 +3,11 @@ package loader
 import (
 	"fmt"
 
+	"github.com/Catizard/lampghost/internal/data"
 	"github.com/Catizard/lampghost/internal/data/rival"
 	"github.com/Catizard/lampghost/internal/data/score"
 	"github.com/Catizard/lampghost/internal/sqlite"
+	"github.com/guregu/null/v5"
 )
 
 var LR2DataLoader RivalDataLoader = newLR2DataLoader()
@@ -21,7 +23,7 @@ func (l *lr2DataLoader) Interest(r *rival.RivalInfo) bool {
 	return r.LR2UserDataPath.Valid
 }
 
-func (l *lr2DataLoader) Load(r *rival.RivalInfo) ([]*score.CommonScoreLog, error) {
+func (l *lr2DataLoader) Load(r *rival.RivalInfo, filter null.Value[data.Filter]) ([]*score.CommonScoreLog, error) {
 	if !l.Interest(r) {
 		return nil, fmt.Errorf("[LR2DataLoader] cannot load")
 	}
--- internal/data/score/loader/scoreLogLoader.go
+++ internal/data/score/loader/scoreLogLoader.go
@@ -1,12 +1,14 @@
 package loader
 
 import (
+	"github.com/Catizard/lampghost/internal/data"
 	"github.com/Catizard/lampghost/internal/data/rival"
 	"github.com/Catizard/lampghost/internal/data/score"
+	"github.com/guregu/null/v5"
 )
 
 type RivalDataLoader interface {
 	Interest(r *rival.RivalInfo) bool
-	Load(r *rival.RivalInfo) ([]*score.CommonScoreLog, error)
+	Load(r *rival.RivalInfo, filter null.Value[data.Filter]) ([]*score.CommonScoreLog, error)
 	// loadWithFilter(r *rival.RivalInfo, filter ???) ([]*score.CommonScoreLog, error)
 }

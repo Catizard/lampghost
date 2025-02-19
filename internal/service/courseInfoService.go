fad29338a5375c009841f7fa6a701b1f7c326095
Catizard <1185032459@qq.com>
2025-02-19 16:29:47 +0800

    fix(course): fix findCourseInfoList

--- internal/service/courseInfoService.go
+++ internal/service/courseInfoService.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/Catizard/lampghost_wails/internal/dto"
+	"github.com/Catizard/lampghost_wails/internal/entity"
 	"github.com/Catizard/lampghost_wails/internal/vo"
 	"gorm.io/gorm"
 )
@@ -23,14 +24,14 @@ func (s *CourseInfoService) FindCourseInfoList(filter *vo.CourseInfoVo) ([]*dto.
 func findCourseInfoList(tx *gorm.DB, filter *vo.CourseInfoVo) ([]*dto.CourseInfoDto, int, error) {
 	if filter == nil {
 		var ret []*dto.CourseInfoDto
-		if err := tx.Find(&ret).Error; err != nil {
+		if err := tx.Model(&entity.CourseInfo{}).Find(&ret).Error; err != nil {
 			return nil, 0, err
 		}
 		return ret, len(ret), nil
 	}
 
 	var ret []*dto.CourseInfoDto
-	if err := tx.Where(filter.Entity()).Find(&ret).Error; err != nil {
+	if err := tx.Model(&entity.CourseInfo{}).Where(filter.Entity()).Find(&ret).Error; err != nil {
 		return nil, 0, err
 	}
 	return ret, len(ret), nil

d86c7a079ddf15326ebb463a6e73fbb49d05e99a
Catizard <1185032459@qq.com>
2025-02-19 17:08:59 +0800

    feat(Rival): implement basic view

--- frontend/src/views/rivals.vue
+++ frontend/src/views/rivals.vue
@@ -14,12 +14,53 @@
 </template>
 
 <script lang="ts" setup>
-import { DataTableColumns } from 'naive-ui';
-import { RivalInfo } from '../../wailsjs/go/models';
+import { FindRivalInfoList } from '@wailsjs/go/controller/RivalInfoController';
+import { dto } from '@wailsjs/go/models';
+import * as dayjs from 'dayjs';
+import { DataTableColumns, useNotification } from 'naive-ui';
+import { ref } from 'vue';
+
+const notification = useNotification();
 
 const pagination = false as const;
+const columns = createColumns();
+const data = ref<Array<dto.RivalInfoDto>>([]);
+const loading = ref<boolean>(false);
 
-function createColumns(): DataTableColumns<dto.RivalInfo> {
-
+function createColumns(): DataTableColumns<dto.RivalInfoDto> {
+	return [
+		{ title: "Name", key: "Name" },
+		{ title: "Play Count", key: "PlayCount" },
+		{ title: "Scorelog.db File Path", key: "ScoreLogPath", ellipsis: true, maxWidth: "150px" },
+		{ title: "SongData.db File Path", key: "SongDataPath", ellipsis: true, maxWidth: "150px" },
+		{ 
+			title: "Last Sync Time",
+			key: "UpdateAt",
+			render: row => {
+				return dayjs(row.UpdatedAt).format('YYYY-MM-DD HH:mm:ss')
+			}
+		}
+	]
 }
+
+function loadData() {
+	loading.value = true;
+	FindRivalInfoList()
+		.then(result => {
+			if (result.Code != 200) {
+				return Promise.reject(result.Msg)
+			}
+			data.value = [...result.Rows];
+		}).catch(err => {
+			notification.error({
+				content: err,
+				duration: 3000,
+				keepAliveOnHover: true
+			})
+		}).finally(() => {
+			loading.value = false;
+		});
+}
+
+loadData();
 </script>

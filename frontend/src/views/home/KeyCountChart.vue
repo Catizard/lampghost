bc0b3e6b5415efa7a39d9a6a6e801c9fd3efb8bb
Catizard <1185032459@qq.com>
2025-05-17 15:24:31 +0800

    fix(keyCountChart): no updates when updating "year"

--- frontend/src/views/home/KeyCountChart.vue
+++ frontend/src/views/home/KeyCountChart.vue
@@ -1,6 +1,8 @@
 <template>
-  <calendar-heatmap :values="data" :endDate="endDate" :tooltip-formatter="tooltipFormatter" :no-data-text="'No Play'"
-    style="width: 95%" />
+	<!-- Hack: using `:key=...` for updating heatmap forcefully -->
+	<!-- Honestly, I have no idea why `endDate` is not reactive-->
+	<calendar-heatmap :values="data" :endDate="endDate" :tooltip-formatter="tooltipFormatter" :no-data-text="'No Play'"
+		style="width: 95%" :key="endDate" />
 </template>
 
 <script setup lang="ts">
@@ -13,65 +15,65 @@ import "vue3-calendar-heatmap/dist/style.css";
 import { useSpecifyYearStore } from "@/stores/home";
 
 const props = defineProps<{
-  rivalId?: number;
+	rivalId?: number;
 }>();
 
 const notification = useNotification();
 const specifyYearStore = useSpecifyYearStore();
 const endDate = computed(() => {
-  const current = dayjs();
-  const currentYear = current.get('year').toString();
-  const specifyYear = specifyYearStore.specifyYear;
-  if (currentYear == specifyYear) {
-    return current;
-  }
-  return dayjs(`${specifyYear}-12-31`);
+	const current = dayjs();
+	const currentYear = current.get('year').toString();
+	const specifyYear = specifyYearStore.specifyYear;
+	if (currentYear == specifyYear) {
+		return current;
+	}
+	return dayjs(`${specifyYear}-12-31`);
 })
 
 const data: Ref<Array<any>> = ref([]);
 
 function loadKeyCountData() {
-  QueryUserKeyCountInYear({
-    RivalId: props.rivalId,
-  } as any)
-    .then((result) => {
-      if (result.Code != 200) {
-        return Promise.reject(result.Msg);
-      }
-      console.log(result);
-      data.value = [
-        ...result.Rows.map((row: dto.KeyCountDto) => {
-          return {
-            count: row.KeyCount,
-            date: row.RecordDate,
-          };
-        }),
-      ];
-      console.log(data.value);
-    })
-    .catch((err) => {
-      notification.error({
-        content: err,
-        duration: 3000,
-        keepAliveOnHover: true,
-      });
-    });
+	QueryUserKeyCountInYear({
+		RivalId: props.rivalId,
+	} as any)
+		.then((result) => {
+			if (result.Code != 200) {
+				return Promise.reject(result.Msg);
+			}
+			console.log(result);
+			data.value = [
+				...result.Rows.map((row: dto.KeyCountDto) => {
+					return {
+						count: row.KeyCount,
+						date: row.RecordDate,
+					};
+				}),
+			];
+			console.log(data.value);
+		})
+		.catch((err) => {
+			notification.error({
+				content: err,
+				duration: 3000,
+				keepAliveOnHover: true,
+			});
+		});
 }
 
 function tooltipFormatter(value: {
-  date: Date | string;
-  count: number;
+	date: Date | string;
+	count: number;
 }): string {
-  const { date, count } = value;
-  const mmdd = dayjs(date).format("YYYY-MM-DD");
-  return `${count} notes <br> ${mmdd}`;
+	const { date, count } = value;
+	const mmdd = dayjs(date).format("YYYY-MM-DD");
+	return `${count} notes <br> ${mmdd}`;
 }
 
 watch(
-  () => props.rivalId,
-  (_, newValue) => {
-    loadKeyCountData();
-  },
-  { once: true },
+	() => props.rivalId,
+	(_, newValue) => {
+		loadKeyCountData();
+	},
+	{ once: true },
 );
 </script>

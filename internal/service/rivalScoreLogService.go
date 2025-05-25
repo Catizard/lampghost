265045e1ec9aa2d527d197483a3961cb7bc44d3a
Catizard <1185032459@qq.com>
2025-05-25 15:10:25 +0800

    fix(recentActivity): strict recent activity data construction
    `scorelog.db` file is appending a new log after each play. However, its
    contents are not the final play result but the best state ever. Suppose
    you have played a same song for three times in a row, real clears are
    "Hard", "Normal", "Normal". But in `scorelog.db` it's "Hard", "Hard",
    "Hard". Clearly the second and the third log doesn't represent a new
    clear state change.
    
    Recent activity compoent should display only "new" clears. Therefore,
    this is an unexpected bug.
    
    Add a new filter statement `clear != old_clear` (actually twice) in
    `queryPrevDayScoreLogList` function. Filtering out the logs that not
    represent a new clear state change could solve this problem.

--- internal/service/rivalScoreLogService.go
+++ internal/service/rivalScoreLogService.go
@@ -235,9 +235,9 @@ func queryPrevDayScoreLogList(tx *gorm.DB, filter *vo.RivalScoreLogVo) ([]*dto.R
 	// TODO: This sql is stricted with "clear >= 4", should we make this configurable?
 	maximumDateQuery := tx.Model(&entity.RivalScoreLog{}).
 		Select("date(max(rival_score_log.record_time))").
-		Where("length(rival_score_log.sha256) = 64 and rival_score_log.clear >= 4 and date(record_time) < date(?) and rival_id = ?", filter.EndRecordTime, filter.RivalId)
+		Where("length(rival_score_log.sha256) = 64 and rival_score_log.clear >= 4 and date(record_time) < date(?) and rival_id = ? and clear != old_clear", filter.EndRecordTime, filter.RivalId)
 	var out []*dto.RivalScoreLogDto
-	partial = partial.Where("length(rival_score_log.sha256) = 64 and rival_score_log.clear >= 4 and rival_id = ?", filter.RivalId).Order("rival_score_log.clear desc")
+	partial = partial.Where("length(rival_score_log.sha256) = 64 and rival_score_log.clear >= 4 and rival_id = ? and clear != old_clear", filter.RivalId).Order("rival_score_log.clear desc")
 	if err := partial.Debug().Where("date(rival_score_log.record_time) = (?)", maximumDateQuery).Group("rival_score_log.sha256").Find(&out).Error; err != nil {
 		return nil, 0, eris.Wrap(err, "failed to query prev day score log")
 	}

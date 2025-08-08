ca5de1bb6f53e3b23a31d718ec21f59f1a3ed198
Catizard <1185032459@qq.com>
2025-08-08 11:10:38 +0800

    refactor(rival): label is ambigous

--- frontend/src/views/rivals/RivalAddForm.vue
+++ frontend/src/views/rivals/RivalAddForm.vue
@@ -1,27 +1,27 @@
 <template>
-	<n-modal :loading="loading" v-model:show="show" preset="dialog" :title="t('title.addPlayer')"
-		:positive-text="t('button.submit')" :negative-text="t('button.cancel')" @positive-click="handlePositiveClick"
-		@negative-click="handleNegativeClick" :mask-closable="false">
-		<n-form ref="formRef" :model="formData" :rules="rules">
-			<n-form-item :label="t('form.labelUserName')" path="Name">
-				<n-input v-model:value="formData.Name" :placeholder="t('form.placeholderUserName')" />
-			</n-form-item>
-			<n-form-item :label="t('form.labelScorelogPath')" path="ScoreLogPath">
-				<n-button type="info" @click="chooseFile('Choose scorelog.db', 'scorelogPath')">
-					{{ t('button.chooseFile') }}
-				</n-button>
-				<n-divider vertical />
-				<n-input v-model:value="formData.ScoreLogPath" :placeholder="t('form.placeholderScorelogPath')" />
-			</n-form-item>
-			<n-form-item :label="t('form.labelScoredatalogPath')" path="ScoreDataLogPath">
-				<n-input disabled v-model:value="formData.ScoreDataLogPath"
-					:placeholder="t('form.placeholderScoredatalogPath')" />
-			</n-form-item>
-			<n-form-item :label="t('form.labelSongdataPath')" path="SongDataPath">
-				<n-input disabled v-model:value="formData.SongDataPath" :placeholder="t('form.placeholderSongdataPath')" />
-			</n-form-item>
-		</n-form>
-	</n-modal>
+  <n-modal :loading="loading" v-model:show="show" preset="dialog" :title="t('title.addPlayer')"
+    :positive-text="t('button.submit')" :negative-text="t('button.cancel')" @positive-click="handlePositiveClick"
+    @negative-click="handleNegativeClick" :mask-closable="false">
+    <n-form ref="formRef" :model="formData" :rules="rules">
+      <n-form-item :label="t('form.labelRivalName')" path="Name">
+        <n-input v-model:value="formData.Name" :placeholder="t('form.placeholderRivalName')" />
+      </n-form-item>
+      <n-form-item :label="t('form.labelScorelogPath')" path="ScoreLogPath">
+        <n-button type="info" @click="chooseFile('Choose scorelog.db', 'scorelogPath')">
+          {{ t('button.chooseFile') }}
+        </n-button>
+        <n-divider vertical />
+        <n-input v-model:value="formData.ScoreLogPath" :placeholder="t('form.placeholderScorelogPath')" />
+      </n-form-item>
+      <n-form-item :label="t('form.labelScoredatalogPath')" path="ScoreDataLogPath">
+        <n-input disabled v-model:value="formData.ScoreDataLogPath"
+          :placeholder="t('form.placeholderScoredatalogPath')" />
+      </n-form-item>
+      <n-form-item :label="t('form.labelSongdataPath')" path="SongDataPath">
+        <n-input disabled v-model:value="formData.SongDataPath" :placeholder="t('form.placeholderSongdataPath')" />
+      </n-form-item>
+    </n-form>
+  </n-modal>
 </template>
 
 <script lang="ts" setup>
@@ -33,7 +33,7 @@ import { useI18n } from 'vue-i18n';
 
 const show = defineModel<boolean>("show");
 const emit = defineEmits<{
-	(e: 'refresh'): void
+  (e: 'refresh'): void
 }>();
 
 const { t } = useI18n();
@@ -41,72 +41,72 @@ const loading = ref(false);
 
 const formRef = ref<FormInst | null>(null);
 const formData = ref({
-	Name: null,
-	ScoreLogPath: null,
-	SongDataPath: null,
-	ScoreDataLogPath: null,
+  Name: null,
+  ScoreLogPath: null,
+  SongDataPath: null,
+  ScoreDataLogPath: null,
 });
 const rules = {
-	Name: {
-		required: true,
-		message: t('rule.missingUserName'),
-		trigger: ["input", "blur"],
-	},
-	ScoreLogPath: {
-		required: true,
-		message: t('rule.missingScorelogPath'),
-		trigger: ["input", "blur"],
-	},
-	// SongDataPath: {
-	// 	required: true,
-	// 	message: t('rule.missingSongdataPath'),
-	// 	trigger: ["input", "blur"],
-	// },
-	// ScoreDataLogPath: {
-	//   required: false,
-	// },
+  Name: {
+    required: true,
+    message: t('rule.missingUserName'),
+    trigger: ["input", "blur"],
+  },
+  ScoreLogPath: {
+    required: true,
+    message: t('rule.missingScorelogPath'),
+    trigger: ["input", "blur"],
+  },
+  // SongDataPath: {
+  // 	required: true,
+  // 	message: t('rule.missingSongdataPath'),
+  // 	trigger: ["input", "blur"],
+  // },
+  // ScoreDataLogPath: {
+  //   required: false,
+  // },
 };
 
 function handlePositiveClick(): boolean {
-	loading.value = true;
-	formRef.value
-		?.validate()
-		.then(() => {
-			return AddRivalInfo(formData.value as any)
-				.then(result => {
-					if (result.Code != 200) {
-						return Promise.reject(result.Msg);
-					}
-					show.value = false;
-					emit('refresh')
-				});
-		})
-		.catch(err => window.$notifyError(err))
-		.finally(() => loading.value = false);
-	return false;
+  loading.value = true;
+  formRef.value
+    ?.validate()
+    .then(() => {
+      return AddRivalInfo(formData.value as any)
+        .then(result => {
+          if (result.Code != 200) {
+            return Promise.reject(result.Msg);
+          }
+          show.value = false;
+          emit('refresh')
+        });
+    })
+    .catch(err => window.$notifyError(err))
+    .finally(() => loading.value = false);
+  return false;
 }
 
 function handleNegativeClick() {
-	formData.value.Name = null;
-	formData.value.ScoreLogPath = null;
-	formData.value.SongDataPath = null;
-	formData.value.ScoreDataLogPath = null;
+  formData.value.Name = null;
+  formData.value.ScoreLogPath = null;
+  formData.value.SongDataPath = null;
+  formData.value.ScoreDataLogPath = null;
 }
 
 // target == "scorelogPath" | "songdataPath"
 function chooseFile(title, target: "scorelogPath" | "songdataPath" | "scoredatalogPath") {
-	OpenFileDialog(title)
-		.then(result => {
-			if (result.Code != 200) {
-				return Promise.reject(result.Msg);
-			}
-			if (result.Data != null && result.Data != undefined && result.Data != "") {
-				switch (target) {
-					case "scorelogPath": formData.value.ScoreLogPath = result.Data; break;
-					case "songdataPath": formData.value.SongDataPath = result.Data; break;
-					case "scoredatalogPath": formData.value.ScoreDataLogPath = result.Data; break;
-				}
-			}
-		}).catch(err => window.$notifyError(err));
+  OpenFileDialog(title)
+    .then(result => {
+      if (result.Code != 200) {
+        return Promise.reject(result.Msg);
+      }
+      if (result.Data != null && result.Data != undefined && result.Data != "") {
+        switch (target) {
+          case "scorelogPath": formData.value.ScoreLogPath = result.Data; break;
+          case "songdataPath": formData.value.SongDataPath = result.Data; break;
+          case "scoredatalogPath": formData.value.ScoreDataLogPath = result.Data; break;
+        }
+      }
+    }).catch(err => window.$notifyError(err));
 }
 </script>
--- frontend/src/views/rivals/RivalEditForm.vue
+++ frontend/src/views/rivals/RivalEditForm.vue
@@ -1,37 +1,37 @@
 <template>
-	<n-modal :loading="loading" v-model:show="show" preset="dialog" :title="t('title.editPlayer')"
-		:positive-text="t('button.submit')" :negative-text="t('button.cancel')"
-		@positive-click="handlePositiveClick" @negative-click="handleNegativeClick" :mask-closable="false">
-		<n-form ref="formRef" :model="formData" :rules="rules">
-			<n-form-item :label="t('form.labelUserName')" path="Name">
-				<n-input v-model:value="formData.Name" :placeholder="t('form.placeholderUserName')" />
-			</n-form-item>
-			<n-form-item :label="t('form.labelScorelogPath')" path="ScoreLogPath">
-				<n-button type="info" @click="chooseFile('Choose scorelog.db', 'scorelogPath')">
-					{{ t('button.chooseFile') }}
-				</n-button>
-				<n-divider vertical />
-				<n-input v-model:value="formData.ScoreLogPath" :placeholder="t('form.placeholderScorelogPath')" />
-			</n-form-item>
-			<n-form-item :disabled="!formData.MainUser" :label="t('form.labelSongdataPath')" path="SongDataPath">
-				<n-button :disabled="!formData.MainUser" type="info" @click="chooseFile('Choose songdata.db', 'songdataPath')">
-					{{ t('button.chooseFile') }}
-				</n-button>
-				<n-divider vertical />
-				<n-input :disabled="!formData.MainUser" v-model:value="formData.SongDataPath"
-					:placeholder="t('form.placeholderSongdataPath')" />
-			</n-form-item>
-			<n-form-item :label="t('form.labelScoredatalogPath')" path="ScoreDataLogPath">
-				<n-button :disabled="!formData.MainUser" type="info"
-					@click="chooseFile('Choose scoredatalog.db', 'scoredatalogPath')">
-					{{ t('button.chooseFile') }}
-				</n-button>
-				<n-divider vertical />
-				<n-input :disabled="!formData.MainUser" v-model:value="formData.ScoreDataLogPath"
-					:placeholder="t('form.placeholderScoredatalogPath')" />
-			</n-form-item>
-		</n-form>
-	</n-modal>
+  <n-modal :loading="loading" v-model:show="show" preset="dialog" :title="t('title.editPlayer')"
+    :positive-text="t('button.submit')" :negative-text="t('button.cancel')" @positive-click="handlePositiveClick"
+    @negative-click="handleNegativeClick" :mask-closable="false">
+    <n-form ref="formRef" :model="formData" :rules="rules">
+      <n-form-item :label="t('form.labelRivalName')" path="Name">
+        <n-input v-model:value="formData.Name" :placeholder="t('form.placeholderRivalName')" />
+      </n-form-item>
+      <n-form-item :label="t('form.labelScorelogPath')" path="ScoreLogPath">
+        <n-button type="info" @click="chooseFile('Choose scorelog.db', 'scorelogPath')">
+          {{ t('button.chooseFile') }}
+        </n-button>
+        <n-divider vertical />
+        <n-input v-model:value="formData.ScoreLogPath" :placeholder="t('form.placeholderScorelogPath')" />
+      </n-form-item>
+      <n-form-item :disabled="!formData.MainUser" :label="t('form.labelSongdataPath')" path="SongDataPath">
+        <n-button :disabled="!formData.MainUser" type="info" @click="chooseFile('Choose songdata.db', 'songdataPath')">
+          {{ t('button.chooseFile') }}
+        </n-button>
+        <n-divider vertical />
+        <n-input :disabled="!formData.MainUser" v-model:value="formData.SongDataPath"
+          :placeholder="t('form.placeholderSongdataPath')" />
+      </n-form-item>
+      <n-form-item :label="t('form.labelScoredatalogPath')" path="ScoreDataLogPath">
+        <n-button :disabled="!formData.MainUser" type="info"
+          @click="chooseFile('Choose scoredatalog.db', 'scoredatalogPath')">
+          {{ t('button.chooseFile') }}
+        </n-button>
+        <n-divider vertical />
+        <n-input :disabled="!formData.MainUser" v-model:value="formData.ScoreDataLogPath"
+          :placeholder="t('form.placeholderScoredatalogPath')" />
+      </n-form-item>
+    </n-form>
+  </n-modal>
 </template>
 
 <script lang="ts" setup>
@@ -44,130 +44,130 @@ import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
 const emit = defineEmits<{
-	(e: 'refresh'): void
+  (e: 'refresh'): void
 }>();
 defineExpose({ open });
 
 const show = ref(false);
 
 function open(rivalID: number) {
-	if (rivalID == null || rivalID == 0) {
-		window.$notifyError(t('message.noChosenRivalError'));
-		show.value = false;
-		return
-	}
+  if (rivalID == null || rivalID == 0) {
+    window.$notifyError(t('message.noChosenRivalError'));
+    show.value = false;
+    return
+  }
 
-	formData.value.ID = rivalID;
-	show.value = true;
-	loading.value = true;
+  formData.value.ID = rivalID;
+  show.value = true;
+  loading.value = true;
 
-	QueryUserInfoByID(rivalID)
-		.then(result => {
-			if (result.Code != 200) {
-				return Promise.reject(result.Msg);
-			}
-			const data: dto.RivalInfoDto = result.Data;
-			formData.value.Name = data.Name;
-			formData.value.ScoreLogPath = data.ScoreLogPath;
-			formData.value.SongDataPath = data.SongDataPath;
-			formData.value.ScoreDataLogPath = data.ScoreDataLogPath;
-			formData.value.MainUser = data.MainUser;
-		}).catch(err => {
-			window.$notifyError(err);
-			show.value = false;
-		}).finally(() => {
-			loading.value = false;
-		});
+  QueryUserInfoByID(rivalID)
+    .then(result => {
+      if (result.Code != 200) {
+        return Promise.reject(result.Msg);
+      }
+      const data: dto.RivalInfoDto = result.Data;
+      formData.value.Name = data.Name;
+      formData.value.ScoreLogPath = data.ScoreLogPath;
+      formData.value.SongDataPath = data.SongDataPath;
+      formData.value.ScoreDataLogPath = data.ScoreDataLogPath;
+      formData.value.MainUser = data.MainUser;
+    }).catch(err => {
+      window.$notifyError(err);
+      show.value = false;
+    }).finally(() => {
+      loading.value = false;
+    });
 }
 
 const loading = ref(false);
 
 const formRef = ref<FormInst | null>(null);
 const formData = ref({
-	ID: 0,
-	Name: null,
-	ScoreLogPath: null,
-	SongDataPath: null,
-	ScoreDataLogPath: null,
-	MainUser: false,
+  ID: 0,
+  Name: null,
+  ScoreLogPath: null,
+  SongDataPath: null,
+  ScoreDataLogPath: null,
+  MainUser: false,
 });
 const rules = reactive({
-	Name: {
-		ignored: false,
-		required: true,
-		message: t('rule.missingUserName'),
-		trigger: ["input", "blur"],
-	},
-	ScoreLogPath: {
-		ignored: false,
-		required: true,
-		message: t('rule.missingScorelogPath'),
-		trigger: ["input", "blur"],
-	},
-	ScoreDataLogPath: {
-		ignored: true,
-		required: true,
-		message: t('rule.missingScoredatalogPath'),
-		trigger: ["input", "blur"],
-	},
-	SongDataPath: {
-		ignored: true,
-		required: true,
-		message: t('rule.missingSongdataPath'),
-		trigger: ["input", "blur"],
-	}
+  Name: {
+    ignored: false,
+    required: true,
+    message: t('rule.missingUserName'),
+    trigger: ["input", "blur"],
+  },
+  ScoreLogPath: {
+    ignored: false,
+    required: true,
+    message: t('rule.missingScorelogPath'),
+    trigger: ["input", "blur"],
+  },
+  ScoreDataLogPath: {
+    ignored: true,
+    required: true,
+    message: t('rule.missingScoredatalogPath'),
+    trigger: ["input", "blur"],
+  },
+  SongDataPath: {
+    ignored: true,
+    required: true,
+    message: t('rule.missingSongdataPath'),
+    trigger: ["input", "blur"],
+  }
 });
 
 function handlePositiveClick(): boolean {
-	loading.value = true;
-	formRef.value
-		?.validate(() => { }, rule => {
-			if (formData.value.MainUser) {
-				return true; // Everything
-			}
-			return (rule as any).ignored == false;
-		})
-		.then(async () => {
-			const result = await UpdateRivalInfo(formData.value as any);
-			if (result.Code != 200) {
-				return Promise.reject(result.Msg);
-			}
-			show.value = false;
-			emit('refresh');
-		})
-		.catch(err => window.$notifyError(err))
-		.finally(() => loading.value = false);
-	return false;
+  loading.value = true;
+  formRef.value
+    ?.validate(() => { }, rule => {
+      if (formData.value.MainUser) {
+        return true; // Everything
+      }
+      return (rule as any).ignored == false;
+    })
+    .then(async () => {
+      const result = await UpdateRivalInfo(formData.value as any);
+      if (result.Code != 200) {
+        return Promise.reject(result.Msg);
+      }
+      show.value = false;
+      emit('refresh');
+    })
+    .catch(err => window.$notifyError(err))
+    .finally(() => loading.value = false);
+  return false;
 }
 
 function handleNegativeClick() {
-	formData.value.ID = 0;
-	formData.value.Name = null;
-	formData.value.ScoreLogPath = null;
-	formData.value.SongDataPath = null;
-	formData.value.ScoreDataLogPath = null;
-	formData.value.MainUser = false;
+  formData.value.ID = 0;
+  formData.value.Name = null;
+  formData.value.ScoreLogPath = null;
+  formData.value.SongDataPath = null;
+  formData.value.ScoreDataLogPath = null;
+  formData.value.MainUser = false;
 }
 
 function chooseFile(title: string, target: "scorelogPath" | "songdataPath" | "scoredatalogPath") {
-	OpenFileDialog(title)
-		.then(result => {
-			if (result.Code != 200) {
-				return Promise.reject(result.Msg);
-			}
-			if (result.Data != null && result.Data != undefined && result.Data != "") {
-				if (target == "scorelogPath") {
-					formData.value.ScoreLogPath = result.Data;
-				} else if (target == "songdataPath") {
-					formData.value.SongDataPath = result.Data;
-				} else if (target == "scoredatalogPath") {
-					formData.value.ScoreDataLogPath = result.Data;
-				}
-			}
-		}).catch(err => window.$notifyError(err));
+  OpenFileDialog(title)
+    .then(result => {
+      if (result.Code != 200) {
+        return Promise.reject(result.Msg);
+      }
+      if (result.Data != null && result.Data != undefined && result.Data != "") {
+        if (target == "scorelogPath") {
+          formData.value.ScoreLogPath = result.Data;
+        } else if (target == "songdataPath") {
+          formData.value.SongDataPath = result.Data;
+        } else if (target == "scoredatalogPath") {
+          formData.value.ScoreDataLogPath = result.Data;
+        }
+      }
+    }).catch(err => window.$notifyError(err));
 }
 
 watch(() => formData.value.MainUser, newValue => {
-	rules.SongDataPath.required = newValue;
+  rules.SongDataPath.required = newValue;
 })
 </script>

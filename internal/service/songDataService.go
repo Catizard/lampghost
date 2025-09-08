5be40ef9494dd4f5f5b11798bd59292c5be74b28
Catizard <1185032459@qq.com>
2025-09-08 20:53:45 +0800

    feat: convert non utf-8 enconding when generating lr2 user's songdata.db file

--- internal/service/songDataService.go
+++ internal/service/songDataService.go
@@ -1,8 +1,14 @@
 package service
 
 import (
+	"io"
+	"strings"
+
 	"github.com/Catizard/bmscanner"
 	"github.com/Catizard/lampghost_wails/internal/entity"
+	"github.com/charmbracelet/log"
+	"github.com/saintfish/chardet"
+	"golang.org/x/net/html/charset"
 	"gorm.io/gorm"
 )
 
@@ -36,9 +42,45 @@ func (s *SongDataService) BuildSongData(directories []string) error {
 	if err != nil {
 		return err
 	}
+	// Try converting encoding if necessary
+	if len(models) == 0 {
+		return nil
+	}
+	any := models[0]
+	detector := chardet.NewTextDetector()
+	r, err := detector.DetectBest([]byte(any.Title))
+	cset := ""
+	if err != nil {
+		log.Infof("failed to detect charset: %s, skipping auto convert steps", err)
+	} else {
+		cset = r.Charset
+	}
+	log.Debugf("cset=%s", cset)
 	songData := make([]*entity.SongData, 0)
 	for _, model := range models {
+		if cset != "" {
+			model.Genre = convEncoding(cset, model.Genre)
+			model.Title = convEncoding(cset, model.Title)
+			model.SubTitle = convEncoding(cset, model.SubTitle)
+			model.Artist = convEncoding(cset, model.Artist)
+			model.SubArtist = convEncoding(cset, model.SubArtist)
+		}
 		songData = append(songData, entity.NewSongDataFromBMSModel(model))
 	}
+	log.Debugf("songdata length=%d", len(songData))
 	return s.db.CreateInBatches(songData, DEFAULT_BATCH_SIZE).Error
+}
+
+// Convert a string from encoding 'ch' to utf-8
+// Keep slient when having error
+func convEncoding(ch, str string) string {
+	r, err := charset.NewReader(strings.NewReader(str), ch)
+	if err != nil {
+		return str
+	}
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return str
+	}
+	return string(b)
 }
